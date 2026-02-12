458ba999c6c1f56fb8b577b2997723bbfed81f42
Alen Peni <134277649+Kaizen-BT@users.noreply.github.com>
2026-02-12 14:28:35 +1000

    added-renderer: React-Router (#1)
    React router has been chosen as the renderer as it is what I am most familiar with

--- packages/renderer/src/main.tsx
+++ packages/renderer/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createHashRouter, RouterProvider } from 'react-router'
 import App from './App.tsx'
+import './index.css'
+
+
+const router = createHashRouter([{ path: "/", Component: App }])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
