a9451662dffdd830c6301a5a909516d91d8b867f
alias-ktc <alenpeni.info@gmail.com>
2026-02-15 16:47:04 +1000

    biome: Mass formatting and linting
    Formatted and linted the entire project using biome

--- packages/renderer/src/main.tsx
+++ packages/renderer/src/main.tsx
@@ -1,11 +1,8 @@
-import { createRoot } from 'react-dom/client'
-import { createHashRouter, RouterProvider } from 'react-router'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router";
+import App from "./App.tsx";
+import "./index.css";
 
+const router = createHashRouter([{ path: "/", Component: App }]);
 
-const router = createHashRouter([{ path: "/", Component: App }])
-
-createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
-)
+createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
