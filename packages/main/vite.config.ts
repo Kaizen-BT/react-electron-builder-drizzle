1c3b4cfb9be5ab14c2b5ebcf975c35297096eb85
alias-ktc <alenpeni.info@gmail.com>
2026-02-17 07:44:31 +1000

    Main package HMR developement
    Added type predicate to narrow down Plugin from Plugin options

--- packages/main/vite.config.ts
+++ packages/main/vite.config.ts
@@ -1,7 +1,7 @@
 import { spawn } from "node:child_process";
 import { getNodeMajorVersion } from "@app/electron-versions";
 import electronPath from "electron";
-import { defineConfig, Plugin } from "vite";
+import { defineConfig, Plugin, PluginOption } from "vite";
 
 export default defineConfig({
   build: {
@@ -24,3 +24,32 @@ export default defineConfig({
   },
   // TODO: Add plugins
 });
+
+/**
+ * Type Predicate to help narrow down types of PluginOptions
+ * NOTE: This is likely not robust enough but is sufficient
+ * for the use case of this application
+ */
+function isPlugin(plugin: PluginOption): plugin is Plugin {
+  return (plugin as Plugin).name !== undefined;
+}
+
+function handleHotReload(): Plugin {
+  return {
+    name: "@app/main-process-hot-reload",
+
+    config(config, env) {
+      if (env.mode !== "development") {
+        return;
+      }
+
+      const rendererWatchServerProvider = config.plugins
+        ?.filter((p) => isPlugin(p))
+        .find((plugin) => plugin.name === "@app/renderer-watch-server-provider");
+
+      if (!rendererWatchServerProvider) {
+        throw new Error("Renderer watch server provider not found");
+      }
+    },
+  };
+}
